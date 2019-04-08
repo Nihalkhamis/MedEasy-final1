abfab0cb6fe22645d4b2b9748fc4868d61e9c128
Nihal Yasser <nihalyasser23@gmail.com>
2019-04-08 14:19:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nihal/medeasy/AssessmentSheet2.java
@@ -0,0 +1,13 @@
+package com.example.nihal.medeasy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssessmentSheet2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessment_sheet2);
+    }
+}
